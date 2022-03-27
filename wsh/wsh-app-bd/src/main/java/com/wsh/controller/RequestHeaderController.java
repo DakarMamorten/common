c44a01ad813895a24992998ac12b2e0adb9b8e3f
Vladyslav paun <vladyslav.paun@sdkfinance.com>
2022-03-27 15:43:12 +0200

    Zadanie5 Nagłówki HTTP - analiza zawartości

---
+++ wsh/wsh-app-bd/src/main/java/com/wsh/controller/RequestHeaderController.java
@@ -0,0 +1,24 @@
+package com.wsh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author "Vladyslav Paun"
+ */
+@RestController
+public class RequestHeaderController {
+
+    @GetMapping("/RequestHeader")
+    @ResponseBody
+    public String exampleRequestHeader(@RequestHeader("User-Agent") String client,
+                                       @RequestHeader("Accept_language") String language,
+                                       @RequestHeader("Cookie") String cookie,
+                                       @RequestHeader("Host") String host) {
+        return "client: " + client + " , " + "language: " + language + " , " + "cookie: " + cookie + " , " + "host: " + host;
+
+    }
+
+}
