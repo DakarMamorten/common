6c9630cdebb42c95415c52f4ed6d56403c8a4c5c
Vladyslav paun <vladyslav.paun@sdkfinance.com>
2022-03-27 14:29:04 +0200

    Initialise multi-module project

---
+++ wsh/wsh-app-bd/src/main/java/com/wsh/wshappbd/WshAppBdApplication.java
@@ -0,0 +1,14 @@
+package com.wsh.wshappbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshAppBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WshAppBdApplication.class,
+                              args);
+    }
+
+}
