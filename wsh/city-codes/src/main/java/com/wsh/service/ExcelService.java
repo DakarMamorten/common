9d2de46abd11e9ed7e266890b1db52fd9f27c746
Vladyslav paun <vladyslav.paun@sdkfinance.com>
2022-05-19 22:38:53 +0200

    Adjust city codes for llh

--- wsh/city-codes/src/main/java/com/wsh/service/ExcelService.java
+++ wsh/city-codes/src/main/java/com/wsh/service/ExcelService.java
@@ -2,6 +2,7 @@ package com.wsh.service;
 
 import com.wsh.dto.CodeDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Component;
@@ -22,13 +23,21 @@ public class ExcelService {
         List<CodeDto> results = new ArrayList<>();
         try (Workbook workbook = WorkbookFactory.create(new File(FILE_PATH))) {
             Sheet sheet = workbook.getSheetAt(0);
+
             for (Row row : sheet) {
                 Cell code = row.getCell(3);
                 Cell adminCode = row.getCell(5);
-
-                if (codes.contains(adminCode.getStringCellValue())) {
-                    results.add(new CodeDto(code.getStringCellValue(), adminCode.getStringCellValue()));
+                if (code != null && adminCode != null) {
+                    String codeValue = code.getStringCellValue();
+                    String adminCodeValue = adminCode.getStringCellValue();
+                    if (StringUtils.isNoneBlank(codeValue)
+                            && StringUtils.isNoneBlank(adminCodeValue)
+                            && codes.contains(adminCode.getStringCellValue())) {
+                        results.add(new CodeDto(codeValue, adminCode.getStringCellValue()));
+                    }
                 }
+
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
